c8d0fdf0cec23f08c051e3ab7e37e2e869a71460
Gabriel Tlaltilolpa <tlaltilolpillo@gmail.com>
2022-12-20 10:28:36 -0600

    Valida email y pass, falta sesion

--- login.php
+++ login.php
@@ -1,61 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotel 5 estrellas</title>
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    
-    <header class="header inicio">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a class="logo" href="index.php">
-                    <h1 class="logo__nombre">Hotel <span class="logo__bold">5 Estrellas</span></h1>
-                </a>
-
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu responsive">
-                </div>
-
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
-                    <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Habitaciones</a>
-                        <a href="registro.php">Registrarse</a>
-                        <a href="contacto.php">Contacto</a>
-                    </nav>
-                </div>
-            </div> <!--.barra-->
-        </div>
-    </header>
-
-<!---Login-->
-    <form class="formulario">
-        
-        <h1>Login</h1>
-        <div class="contenedor">
-            
-            <div class="input-contenedor">
-            <i class="fas fa-envelope icon"></i>
-            <input type="text" placeholder="Correo Electronico">
-            
-            </div>
-            
-            <div class="input-contenedor">
-            <i class="fas fa-key icon"></i>
-            <input type="password" placeholder="Contraseña">
-            
-            </div>
-            <input type="submit" value="Login" class="button">
-            <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
-            <p>¿No tienes una cuenta? <a class="link" href="registro.php">Registrate </a></p>
-        </div>
-        </form>
-<!---ENDFormulario-->
-
+<?php
+        include './includes/templates/header.php';
+        require './conexion.php';
+        ?>
+    <main class="contenedor seccion">
+    <!---Login-->
+        <form class="formulario" method="POST" action="./validar.php">
+            <fieldset>
+                <h1>Login</h1>
+                
+                    
+                    <div class="input-contenedor">
+                    <i class="fas fa-envelope icon"></i>
+                    <input type="text" name="Correo" placeholder="Correo Electronico">
+                    
+                    </div>
+                    
+                    <div class="input-contenedor">
+                    <i class="fas fa-key icon"></i>
+                    <input type="password" name="password" placeholder="Contraseña">
+                    
+                    </div>
+                    <input type="submit" value="Login" class="button">
+                    <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
+                    <p>¿No tienes una cuenta? <a class="link" href="registro.php">Registrate </a></p>
+                
+            </fieldset>
+            </form>
+    <!---ENDFormulario-->
+    </main>
 <?php
       include './includes/templates/footer.php';
     ?>
---
+++ validar.php
@@ -0,0 +1,31 @@
+<?php
+require './conexion.php';
+$usuario=$_POST['Correo'];
+$contraseña=$_POST['password'];
+//session_start();
+//$_SESSION['usuario']=$usuario;
+
+    echo $usuario."<br/>";
+    echo $contraseña."<br/>";
+
+$exist = validarPass($usuario,$contraseña);
+
+if($exist == "1"){
+    header("location:anuncios.php");
+    ?>
+    
+  <!-- <h1 > AUTENTIFICACION</h1> -->
+  <?php
+    print $exist;
+}else{
+      print '<script language="JavaScript">';
+      print 'alert("Usuario o contraseña equivocada");';
+      print "window.location= 'login.php'";
+      print '</script>';
+      //header("location:login.php");
+    ?>
+    
+  <!-- <h1 >ERROR DE AUTENTIFICACION</h1> -->
+  <?php
+  echo $exist;
+}
