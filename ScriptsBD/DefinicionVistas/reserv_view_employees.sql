b2cde49a42bc3b9bb1b565eff9b8243d35aa914c
Luis.Antfra <luis.antfra@gmail.com>
2023-01-16 00:47:15 -0600

    se añade vista para consultar las reservaciones que hizo un usuario

---
+++ ScriptsBD/DefinicionVistas/reserv_view_employees.sql
@@ -0,0 +1,12 @@
+create view reserv_view_employees
+as
+select Empleados.idEmpl,
+CONCAT(Empleados.Nombre,' ',Empleados.ApP,' ',Empleados.ApM) empleado_full_name,
+Reservaciones.idHues,
+Reservaciones.idHab,
+Reservaciones.beg_dte,
+Reservaciones.end_dte,
+Reservaciones.CostoF
+from 
+Empleados join Reservaciones 
+on Empleados.idEmpl = Reservaciones.idEmpl;
