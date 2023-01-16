4ee9487f77b4311210fb69195faf2b2ea53f209b
Luis.Antfra <luis.antfra@gmail.com>
2023-01-15 23:40:01 -0600

    Se modifica SP de crear reservación, valida el precio final con una función

--- ScriptsBD/DefiniciónSP/CreateReservacion.sql
+++ ScriptsBD/DefiniciónSP/CreateReservacion.sql
@@ -5,9 +5,11 @@ create Procedure CreateReservation
  @idHues int,
  @idHab int,
  @idEmpl int,
- @flg bit
+ @flg bit = null
  as
- /*En la variable flg determina si el rango de inicio y fin de las reservaciones existentes para la habitación coinciden con la que se intenta crear*/
+ declare @precio_f money;
+ set @precio_f= (select dbo.calcular_precio(@beg_dte,@end_dte,@idHab));
+ /*En la variable flg determina si el rango de inicio y fin de las reservaciones existentes para la habitaciï¿½n coinciden con la que se intenta crear*/
  select @flg =(select 1 val from Reservaciones
 where idHab=@idHab 
 and( @beg_dte between  beg_dte
@@ -17,15 +19,10 @@ and end_dte))
 
 if @flg is null 
 begin
-insert into Reservaciones (idHues,idHab,idEmpl,beg_dte,end_dte) values(@idHues,@idHab,@idEmpl,convert(datetime,@beg_dte, 103),convert(datetime,@end_dte, 103))
-select 'Reservación exitosa' value
+insert into Reservaciones (idHues,idHab,idEmpl,beg_dte,end_dte,CostoF) values(@idHues,@idHab,@idEmpl,convert(datetime,@beg_dte, 103),convert(datetime,@end_dte, 103),@precio_f)
+select 'Reservacion exitosa' value
 end
 else
 begin
-select 'Habitación no disponible' value
+select 'Habitacion no disponible' value
 end
-
-
-
-
-
