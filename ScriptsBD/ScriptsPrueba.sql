e580b6c69c3ffa0c51e3944333f0eead54bd507e
Luis.Antfra <luis.antfra@gmail.com>
2023-01-16 00:45:39 -0600

    Se añade validación para las reservaciones en los scripts de prueba

--- ScriptsBD/ScriptsPrueba.sql
+++ ScriptsBD/ScriptsPrueba.sql
@@ -8,3 +8,8 @@ Función para calcular el costo de una reservación
 fecha inicial,fecha,final,tipo_habitación
 */
 select dbo.calcular_precio('20230115','20230120',1);
+/*
+Función para validar la disponibilidad de una habitación 
+fecha inicial,fecha final,id_habitacion
+*/
+select dbo.validar_fecha_reservacion('20230115','20230120',1)
